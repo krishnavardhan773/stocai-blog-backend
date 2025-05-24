105394efff9280178f265be88d096dccf2d83d2b
Krishna Vardhan <krishnavardhan@Krishnas-MacBook-Air.local>
2025-05-24 16:59:27 +0530

    Added /api/blogs/ endpoint + migrations

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,43 +1,21 @@
+# blogs/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BlogPostViewSet
+from .views import (
+    BlogPostViewSet,
+    CommentListCreateView,
+    StorySubmissionCreateView,
+    FeedbackCreateView,
+)
 
+# 1. Set up router for BlogPostViewSet
 router = DefaultRouter()
-router.register(r'blogs', BlogPostViewSet)
+router.register(r'blogs', BlogPostViewSet)  # This handles /blogs/, /blogs/<id>/ etc.
 
+# 2. Add all routes
 urlpatterns = [
-    path('', include(router.urls)),
-]
-
-from .views import CommentListCreateView
-
-urlpatterns += [
-    path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
-]
-
-from .views import StorySubmissionCreateView
-
-urlpatterns += [
-    path('stories/', StorySubmissionCreateView.as_view(), name='story-submit'),
-]
-
-from .views import FeedbackCreateView
-
-urlpatterns += [
-    path('feedback/', FeedbackCreateView.as_view(), name='feedback'),
-]
-
-# blogs/urls.py
-from django.urls import path
-from .views import CommentList
-
-urlpatterns = [
-    path("api/comments/", CommentList.as_view(), name="comment-list"),
-]
-
-from django.urls import path
-from .views import BlogPostList
-
-urlpatterns = [
-    path("api/blogs/", BlogPostList.as_view(), name="blog-list"),
+    path('api/', include(router.urls)),  # This makes /api/blogs/ available
+    path('api/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('api/stories/', StorySubmissionCreateView.as_view(), name='story-submit'),
+    path('api/feedback/', FeedbackCreateView.as_view(), name='feedback'),
 ]
